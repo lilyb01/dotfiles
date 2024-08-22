f3b5e4ea343fb01b801eb59f940b6ab7ffb1ae2d
lilyb01 <rainiskies17@gmail.com>
2024-08-22 04:22:30 -0400

    cantata home.nix update (i forgor)

--- hosts/cantata/home.nix
+++ hosts/cantata/home.nix
@@ -1,8 +1,8 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
     custom = {
         editors.vscode = true;
         gnome.favorites = lib.mkAfter [];
-        gnome.extensions = [
+        gnome.extensions = with pkgs.gnomeExtensions; [
           user-themes
           dash-to-dock
           window-title-is-back
@@ -23,6 +23,19 @@
           status-area-horizontal-spacing
         ];
     };
+
+      home.file."wallpaper.jpg" = {
+        source = ../../wallpaper/wallpaper.jpg;
+      };
+      home.file."wallpaper_dark.jpg" = {
+        source = ../../wallpaper/wallpaper_dark.jpg;
+      };
+      home.file."lockscreen.jpg" = {
+        source = ../../wallpaper/lockscreen.jpg;
+      };
+      home.file."lockscreen-dark.jpg" = {
+        source = ../../wallpaper/lockscreen-dark.jpg;
+      };
 
     home.stateVersion = "24.05";
 }

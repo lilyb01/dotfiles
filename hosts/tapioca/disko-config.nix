a0884e936b9b6a84d5210ad91dd0490e172900b9
Lily B. <lily@buny.plus>
2024-09-04 08:39:29 -0400

    asdfsdfdsf

--- hosts/tapioca/disko-config.nix
+++ hosts/tapioca/disko-config.nix
@@ -8,20 +8,16 @@
           type = "gpt";
           partitions = {
             ESP = {
-              start = "1MiB";
-              end = "500MiB";
-              bootable = true;
-              priority = 1;
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
+                type = "EF00";
+                size = "500M";
+                content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                };
             };
             tapioca_root = {
-              start = "500MiB";
-              end = "100%";
-              part-type = "primary";
+              size = "100%";
               priority = 2;
               content = {
                 type = "filesystem";

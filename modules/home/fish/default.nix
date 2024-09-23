e9d1aaccb0ea38441d3f919774b74e3a8235f88e
Lily B. <lily@buny.plus>
2024-09-23 01:33:43 -0400

    sdfsdfwer

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -17,20 +17,11 @@
         ];
     };
 
-    programs.bash = {
-        interactiveShellInit = ''
-            if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-            then
-            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-            fi
-        '';
-    };
-
     programs.starship = {
         enable = true;
         # Configuration written to ~/.config/starship.toml
         settings = {
+            format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)$character";
         # add_newline = false;
 
         # character = {

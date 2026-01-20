90ac204607570c810becc4bfcf84fc374261ca96
Lily B. <lily@buny.plus>
2026-01-20 16:23:06 -0500

    klsdfjkdsjkl

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.custom.gaming;
+  
 in
 {
   options.custom.gaming.enable = lib.mkEnableOption "gaming";
@@ -8,13 +9,21 @@ in
   config = lib.mkIf cfg.enable {
     hardware.graphics.enable = true;
 
-    programs.steam = {
+    programs.steam = let
+      patchedBwrap = pkgs.bubblewrap.overrideAttrs (o: {
+        patches = (o.patches or []) ++ [
+          ./vr/patching/bwrap.patch
+        ];
+      });
+    in {
       enable = true;
       package = pkgs.steam.override {
-        # force proton to use wayland
-        #export PROTON_ENABLE_WAYLAND=1
-        # FIXME: This allowes xrizer & opencomposite to work without something in home
-        #export XR_RUNTIME_JSON=${config.services.monado.package}/share/openxr/1/openxr_monado.json
+        buildFHSEnv = (args: ((pkgs.buildFHSEnv.override {
+          bubblewrap = patchedBwrap;
+        }) (args // {
+          extraBwrapArgs = (args.extraBwrapArgs or []) ++ [ "--cap-add ALL" ];
+        })));
+
         extraProfile = ''
           # fixes timezones on vrchat
           unset TZ
@@ -30,6 +39,30 @@ in
         protontricks
       ];
     };
+
+
+    #programs.steam = {
+    #  enable = true;
+    #  package = pkgs.steam.override {
+    #    # force proton to use wayland
+    #    #export PROTON_ENABLE_WAYLAND=1
+    #    # FIXME: This allowes xrizer & opencomposite to work without something in home
+    #    #export XR_RUNTIME_JSON=${config.services.monado.package}/share/openxr/1/openxr_monado.json
+    #    extraProfile = ''
+    #      # fixes timezones on vrchat
+    #      unset TZ
+    #      # allows monado to be used
+    #      export PRESSURE_VESSEL_FILESYSTEMS_RW=$XDG_RUNTIME_DIR/monado_comp_ipc
+    #    '';
+    #  };
+    #
+    #  remotePlay.openFirewall = true;
+    #  localNetworkGameTransfers.openFirewall = true;
+    #  extraCompatPackages = with pkgs; [
+    #    proton-ge-bin
+    #    protontricks
+    #  ];
+    #};
 
 #    programs.nix-ld = {
 #      enable = true;
